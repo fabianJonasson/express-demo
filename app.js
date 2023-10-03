ab569e89a6ebcb2674142595ce3ceaaa1b9f4e5e
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-10-03 13:31:34 +0200

    added a route /about to a new page, all 3 initial tests now pass

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const aboutRouter = require('./routes/about');
 
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
