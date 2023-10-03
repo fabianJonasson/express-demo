79daf24494815822591016e15bf77ce9ce6cf67a
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-10-03 13:41:23 +0200

    added describes to test file in order to group new and old tests

--- app.test.js
+++ app.test.js
@@ -1,23 +1,29 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('I am alive and responding', done => {
-    request(app)
-    .get('/')
-    .expect(200)
-    .end(done)
+describe('base', () => {
+    test('I am alive and responding', done => {
+        request(app)
+        .get('/')
+        .expect(200)
+        .end(done)
+    });
+    
+    test('GET /', done => {
+        request(app)
+        .get('/about')
+        .expect(200)
+        .end(done)
+    });
+    
+    test('GET jibberish', done => {
+        request(app)
+        .get('/asjdgajdsg')
+        .expect(404)
+        .end(done)
+    });
 });
 
-test('GET /', done => {
-    request(app)
-    .get('/about')
-    .expect(200)
-    .end(done)
-});
+describe('extra', () => {
 
-test('GET jibberish', done => {
-    request(app)
-    .get('/asjdgajdsg')
-    .expect(404)
-    .end(done)
 });
